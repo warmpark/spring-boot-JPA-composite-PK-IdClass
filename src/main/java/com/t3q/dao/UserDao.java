684dfdc1adc16178b950e57a497972f836f80223
byunghoon park <warmpark@t3q.com>
2016-02-09 05:06:53 +0900

    update DAO

--- src/main/java/com/t3q/dao/UserDao.java
+++ src/main/java/com/t3q/dao/UserDao.java
@@ -6,15 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.t3q.jpa.model.User;
 
-/**
- * A DAO for the entity User is simply created by extending the CrudRepository
- * interface provided by spring. The following methods are some of the ones
- * available from such interface: save, delete, deleteAll, findOne and findAll.
- * The magic is that such methods must not be implemented, and moreover it is
- * possible create new query methods working only by defining their signature!
- * 
- * @author netgloo
- */
+
 @Transactional
 public interface UserDao extends JpaRepository<User, String> {
 
